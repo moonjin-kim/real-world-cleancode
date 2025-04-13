ea43a8696e3378ffa8c67751ff40194d96a12de8
moonjin-kim <ace39418@gmail.com>
2025-04-13 20:45:48 +0900

    add: user entity 생성

--- src/main/java/com/moonjin/realworld/user/domain/User.java
+++ src/main/java/com/moonjin/realworld/user/domain/User.java
@@ -1,5 +1,29 @@
 package com.moonjin.realworld.user.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Builder;
+
+@Entity
 public class User {
-    private String id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email;
+    private String password;
+    private String username;
+    private String bio;
+    private String image;
+
+    @Builder
+    public User(String email, String password, String username, String bio, String image) {
+        this.email = email;
+        this.password = password;
+        this.username = username;
+        this.bio = bio;
+        this.image = image;
+    }
 }
