1081a97ed47426a16072162547119e0b81a860d9
moonjin-kim <ace39418@gmail.com>
2025-05-06 17:54:43 +0900

    rft: pageRequest 분리

--- src/main/java/com/moonjin/realworld/dto/request/Page.java
+++ src/main/java/com/moonjin/realworld/dto/request/Page.java
@@ -1,15 +1,28 @@
 package com.moonjin.realworld.dto.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 
 import static java.lang.Math.min;
 
-@Builder
 @Data
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Page {
+    private static final int MAX_PAGE = 999;
+    private static final int MAX_SIZE = 2000;
+    @Builder.Default
+    private Integer limit = 10;
+    @Builder.Default
+    private Integer offset = 1;
 
-
+    public long getPageNum() {
+        return (long) (limit - 1) * min(offset, MAX_SIZE);
+    }
 }
--- src/main/java/com/moonjin/realworld/dto/request/article/FeedParam.java
+++ src/main/java/com/moonjin/realworld/dto/request/article/FeedParam.java
@@ -1,20 +1,13 @@
 package com.moonjin.realworld.dto.request.article;
 
 import com.moonjin.realworld.dto.request.Page;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
 import static java.lang.Math.min;
 
 @Getter
 @SuperBuilder
-@NoArgsConstructor
-@AllArgsConstructor
-public class ArticleParam extends Page {
-    String tag;
-    String author;
-    String favorited;
+public class FeedParam extends Page {
 }
